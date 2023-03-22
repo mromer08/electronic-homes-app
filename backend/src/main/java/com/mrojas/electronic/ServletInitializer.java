20cb25b71745b157b93184ed3e1f393a507b404d
Manuel Rojas <manuerjs70@gmail.com>
2023-03-21 19:52:43 -0600

    Add init files for spring project

---
+++ backend/src/main/java/com/mrojas/electronic/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.mrojas.electronic;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ElectronicApplication.class);
+	}
+
+}

ea0ac53e8421666e9deac4ae2c0331f5b26db4d6
Manuel Rojas <manuerjs70@gmail.com>
2023-03-27 00:27:44 -0600

    feat: add Venta & detalleVenta java files for models

---
+++ backend/src/main/java/com/mrojas/electronic/model/sale/DetalleVenta.java
@@ -0,0 +1,45 @@
+package com.mrojas.electronic.model.sale;
+
+import com.mrojas.electronic.model.inventory.Producto;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Detalleventa", schema = "controlventas")
+public class DetalleVenta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "id_venta", referencedColumnName = "id")
+    private Venta venta;
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "id_producto", referencedColumnName = "id")
+    private Producto producto;
+    private Integer cantidad;
+
+    @Transient
+    @Builder.Default
+    private Double total = 0.0;
+
+    public Double getTotal(){
+        this.setTotal(this.cantidad*this.producto.getPrecio());
+        return this.total;
+    }
+}
---
+++ backend/src/main/java/com/mrojas/electronic/model/sale/Venta.java
@@ -0,0 +1,49 @@
+package com.mrojas.electronic.model.sale;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.mrojas.electronic.model.customer.Cliente;
+import com.mrojas.electronic.model.employee.Empleado;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.persistence.Transient;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Venta", schema = "controlventas")
+public class Venta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "id_empleado", referencedColumnName = "id")
+    private Empleado empleado;
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "id_cliente", referencedColumnName = "id")
+    private Cliente cliente;
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-mm-dd")
+    private Date fecha;
+
+    @Transient
+    @Builder.Default
+    Double total = 0.0;
+}

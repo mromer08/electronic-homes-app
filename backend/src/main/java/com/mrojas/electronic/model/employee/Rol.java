77a6f92306a1d0a96410ee7824c92114989abd5e
Manuel Rojas <manuerjs70@gmail.com>
2023-03-26 19:58:44 -0600

    feat: add rol.java file

---
+++ backend/src/main/java/com/mrojas/electronic/model/employee/Rol.java
@@ -0,0 +1,24 @@
+package com.mrojas.electronic.model.employee;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Rol", schema = "controlpersonal")
+public class Rol {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    String nombre;
+}
